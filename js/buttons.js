85d0516799eae2a1011c5139cda5d4667b024956
Calum Flanagan <calum.flanagan@go.edgehill.ac.uk>
2017-03-21 19:21:43 +0000

    Added buttons for the different options instead of dropdowns. Added increment and decrement buttons for the quantity.

---
+++ js/buttons.js
@@ -0,0 +1,10 @@
+document.getElementById('option1').style.backgroundColor='';
+document.getElementById('option2').style.backgroundColor='';
+
+document.getElementById('option1').onclick=function() {
+  document.getElementById('option1').style.backgroundColor='#d81c25';
+}
+
+document.getElementById('option2').onclick=function() {
+  document.getElementById('option2').style.backgroundColor='#d81c25';
+}
---
+++ js/plusMinus.js
@@ -0,0 +1,21 @@
+/**
+* Gabe Calabro
+* http://jsfiddle.net/7va04ehz/
+*/
+
+
+function incrementValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+}
+
+function decrementValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('number').value = value;
+}
